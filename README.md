a7407a30a40947b50f8beea68d4de59490d35af4
Naoya Murakami <visionary.incident@gmail.com>
2016-06-26 04:48:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+# Groonga Index Selector plugin
+
+config``index-selector.table``に設定されたテーブルのキーで検索された場合、そのテーブルにある同名のインデックスを使うようにexpressionを書き換えます。
+
+例えば、https://github.com/naoa/groonga-tokenizer-white を使って、特定のワードのみのWITH_POSITIONなしのインデックスを作ることによりそのワードの検索を高速化することを目的にしてます。
+
+## Install
+
+Install libgroonga-dev.
+
+Build this function.
+
+    % sh autogen.sh
+    % ./configure
+    % sudo make install
+
+## Usage
+
+Register `expression_rewriters/index_selector`:
+
+    % groonga DB
+    > plugin_register expression_rewriters/index_selector
+
+Create expression_rewriters table
+
+```
+table_create expression_rewriters TABLE_HASH_KEY ShortText
+column_create expression_rewriters plugin_name COLUMN_SCALAR Text
+load --table expression_rewriters
+[
+{"_key": "index_selector", "plugin_name": "expression_rewriters/index_selector"}
+]
+```
+
+Set use index selector table to config
+
+```
+config_set index-selector.table white_words
+```
+
+Set target key to table
+
+```
+table_create white_words TABLE_PAT_KEY ShortText --default_tokenizer TokenBigram --normalizer NormalizerAuto
+load --table white_words
+[
+{"_key": "groonga"}
+]
+```
+
+Create data table and same index to index selector table(``white_wods``)
+
+```
+table_create Entries TABLE_NO_KEY
+column_create Entries title COLUMN_SCALAR ShortText
+column_create Entries body COLUMN_SCALAR ShortText
+
+table_create Terms TABLE_PAT_KEY ShortText --default_tokenizer TokenBigram
+column_create Terms title COLUMN_INDEX|WITH_POSITION Entries title
+load --table Entries
+[
+{"title": "Groonga and MySQL", "body": "Groonga PostgreSQL"}
+]
+
+column_create white_words title COLUMN_INDEX|WITH_POSITION Entries title
+```
+
+Now you can search `groonga` word using white_words.title index.
+
+```
+select Entries --filter 'title @ "groonga"'
+
+[
+  [
+    0,
+    0.0,
+    0.0
+  ],
+  [
+    [
+      [
+        1
+      ],
+      [
+        [
+          "_id",
+          "UInt32"
+        ],
+        [
+          "body",
+          "ShortText"
+        ],
+        [
+          "title",
+          "ShortText"
+        ]
+      ],
+      [
+        1,
+        "Groonga PostgreSQL",
+        "Groonga and MySQL"
+      ]
+    ]
+  ]
+]
+```
+
+## License
+
+Public domain. You can copy and modify this project freely.
---
+++ expression_rewriters/index_selector.rb
@@ -0,0 +1,122 @@
+module Groonga
+  module ExpressionRewriters
+    class IndexSelector < ExpressionRewriter
+      register "index_selector"
+
+      def rewrite
+        p "start expression"
+        p @expression
+        optimized_lexicon_name = Config["index-selector.table"]
+        return @expression unless optimized_lexicon_name
+
+        builder = ExpressionTreeBuilder.new(@expression)
+        root_node = builder.build
+        variable = @expression[0]
+        table = context[variable.domain]
+
+        @optimized_lexicon = context[optimized_lexicon_name]
+
+        p "original root"
+        p root_node
+        optimized_root_node = optimize_node(table, root_node)
+        p "optimized root"
+        p optimized_root_node
+        rewritten = Expression.create(table)
+        optimized_root_node.build(rewritten)
+        p "finish rewritten expression"
+        p rewritten
+        rewritten
+      end
+
+      private
+      def optimize_node(table, node)
+        case node
+        when ExpressionTree::LogicalOperation
+          optimized_sub_nodes = node.nodes.collect do |sub_node|
+            optimize_node(table, sub_node)
+          end
+          ExpressionTree::LogicalOperation.new(node.operator,
+                                               optimized_sub_nodes)
+        when ExpressionTree::BinaryOperation
+          if (node.left.is_a?(ExpressionTree::Variable) or
+              node.left.is_a?(ExpressionTree::IndexColumn) or
+              node.left.is_a?(ExpressionTree::Accessor)) and
+              node.right.is_a?(ExpressionTree::Constant)
+            optimized_left = node.left
+            if @optimized_lexicon[node.right.value]
+              if node.left.is_a?(ExpressionTree::Variable) and node.left.column.is_a?(Expression)
+                match_builder = ExpressionTreeBuilder.new(node.left.column)
+                match_column_node = match_builder.build
+                optimized_match_column_node = optimize_match_column_node(table, match_column_node)
+                rewritten = Expression.create(table)
+                optimized_match_column_node.build(rewritten)
+                optimized_left = ExpressionTree::Variable.new(rewritten)
+              else
+                optimized_left = optimize_match_column_node(table, node.left)
+              end
+            end
+
+            if node.left == optimized_left
+              node
+            else
+              ExpressionTree::BinaryOperation.new(node.operator,
+                                                  optimized_left,
+                                                  node.right)
+            end
+          else
+            node
+          end
+        else
+          node
+        end
+      end
+
+      def optimize_match_column_node table, node
+        case node
+        when ExpressionTree::BinaryOperation
+          node
+        when ExpressionTree::Variable
+          optimized_index = nil
+          node.column.indexes.each do |info|
+            next if info.index.source_ids.size != 1
+            index_column_name = info.index.name.split('.').last
+            optimized_index = context["#{@optimized_lexicon.name}.#{index_column_name}"]
+            break if optimized_index
+          end
+          if optimized_index
+            ExpressionTree::IndexColumn.new(optimized_index)
+          else
+            node
+          end
+        when ExpressionTree::IndexColumn
+          index_column_name = node.index_column.name.split('.').last
+          optimized_index = context["#{@optimized_lexicon.name}.#{index_column_name}"]
+          if optimized_index
+            ExpressionTree::IndexColumn.new(optimized_index)
+          else
+            node
+          end
+        when ExpressionTree::Accessor
+          accessor = node.value
+          original_source_ids = accessor.object.source_ids
+          optimized_accessor = @optimized_lexicon.find_column("#{accessor.name}")
+          @expression.take_object(optimized_accessor)
+          new_source_ids = optimized_accessor.object.source_ids
+          if original_source_ids == new_source_ids
+            ExpressionTree::Accessor.new(optimized_accessor)
+          else
+            node
+          end
+        when ExpressionTree::LogicalOperation
+          optimized_sub_nodes = node.nodes.collect do |sub_node|
+            optimize_match_column_node(table, sub_node)
+          end
+          ExpressionTree::LogicalOperation.new(node.operator,
+                                               optimized_sub_nodes)
+        else
+          node
+        end
+      end
+    end
+  end
+end
